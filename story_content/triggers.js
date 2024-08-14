0fa53384649ff0337340f99176d801ab083b3fdc
KausarMohammad <55966879+KausarMohammad@users.noreply.github.com>
2024-08-14 15:57:58 -0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5r37aljaqhb":
+        Script1();
+        break;
+      case "6EB4k1FOPEe":
+        Script2();
+        break;
+      case "5m4KzoiCw3R":
+        Script3();
+        break;
+      case "5ZnjN6UbtGO":
+        Script4();
+        break;
+      case "5ZLWmoX3qj9":
+        Script5();
+        break;
+      case "6QUNpyFwmp6":
+        Script6();
+        break;
+      case "6QlfEf1rqcq":
+        Script7();
+        break;
+      case "5hEFcr5tgw1":
+        Script8();
+        break;
+      case "6MJnAyh1YXl":
+        Script9();
+        break;
+      case "68qDXsvU0UA":
+        Script10();
+        break;
+      case "68e1QyGk0K0":
+        Script11();
+        break;
+      case "5fu0QcoOqQT":
+        Script12();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
